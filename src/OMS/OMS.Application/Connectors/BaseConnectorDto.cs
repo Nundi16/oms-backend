e5313ac9baa2c0e1f7195b74637121bf911654cb
Bodnár Sándor <sandor.bodnar84@gmail.com>
2026-06-16 20:08:24 +0200

    feat: PolymorphicJsonOption for BaseConnector

---
+++ src/OMS/OMS.Application/Connectors/BaseConnectorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OMS.Application.Connectors
+{
+    public class BaseConnectorDto
+    {
+        public Guid Id { get; set; }
+        public Guid ParentId { get; set; }
+        public Guid DependantId { get; set; }
+    }
+}
